93bc7c19bc4857740bbd973f739a08bd99dc3f92
Praveen G <pngopali1@gmail.com>
2022-07-17 19:27:51 +0530

    This first code push

---
+++ mongotest1.py
@@ -0,0 +1,15 @@
+import pymongo
+
+#client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://gopalip:mongo1996@cluster0.urrq1.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"Praveen",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
